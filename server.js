620101676bca5221828e183dbd40a6c5b225aa58
Amarnath WebDev <amarnathkumar2021@gmail.com>
2025-07-11 17:39:04 +0530

    Raahi is your smart and reliable ride-sharing app

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import http from "http";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
